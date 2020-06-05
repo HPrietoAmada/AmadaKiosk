f8d4d903c5b879884f0a1735e29126e8edd628ee
Heriberto Prieto <HPrieto@amada.com>
2020-06-05 11:18:42 -0700

    Initial Commit

---
+++ AmadaKiosk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmadaKiosk
+//
+//  Created by HPrietoAmada on 6/5/20.
+//  Copyright © 2020 IT Support. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
